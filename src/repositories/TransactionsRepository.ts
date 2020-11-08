e13fc87458a3f3504f81d717ad14695c17167b16
vitor <vitorn2009@hotmail.com>
2020-11-08 20:04:50 -0300

    solucao

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
     // TODO
+    let income = 0
+
+    let outcome = 0
+
+    this.transactions.map(transaction => {
+      if(transaction.type === 'income'){
+        income += transaction.value
+      }else {
+        outcome += transaction.value
+      }
+    })
+
+    const total = income - outcome
+
+    const balance: Balance = { income, outcome, total}
+
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
+
+    const transaction = new Transaction({ title, value, type })
+
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
